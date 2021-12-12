88d14cc8bcef9275ddaaaa0635f610e2ff782d91
Ariel Dorssi <adorssi@gmail.com>
2021-12-12 16:11:49 -0300

    first commit

---
+++ src/controllers/productsController.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const imagesPath = path.join(__dirname, '../../public/images/products/');
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', {products: products});
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const id = Number(req.params.id);
+		const productToVisit = products.filter( product => product.id === id);
+		res.render('detail', {product: productToVisit[0]});
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form');
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const id = Date.now();
+		const {name, price, discount, category, description} = req.body;
+		const image = req.file.filename;
+		const productObj = {
+			id,
+			name,
+			price: Number(price),
+			discount: Number(discount),
+			category,
+			description,
+			image
+		}
+		products.unshift(productObj);
+		const productJSON = JSON.stringify(products);
+		fs.writeFileSync(productsFilePath, productJSON);
+
+		res.redirect('/products');
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		const id = Number(req.params.id);
+		const productToEdit = products.filter( product => product.id === id);
+		res.render('product-edit-form', {product: productToEdit[0]});
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		const id = Number(req.params.id);
+		let image;
+		const {name, price, discount, category, description} = req.body;
+		products.forEach( product => {
+			if(product.id === id) {
+				if(req.file) {
+					image = req.file.filename;
+					fs.unlink(imagesPath + product.image, (err) => {
+						if(err) throw err;
+						console.log(err);
+					})
+				};
+
+				product.name = name;
+				product.price = Number(price);
+				product.discount = Number(discount);
+				product.category = category;
+				product.description = description;
+				product.image = image || product.image;
+
+				const productJSON = JSON.stringify(products);
+				fs.writeFileSync(productsFilePath, productJSON);
+			}
+		});
+		res.redirect('/products');
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		const id = Number(req.params.id);
+		const updateProducts = products.filter( product => product.id != id);
+		const productJSON = JSON.stringify(updateProducts);
+		fs.writeFileSync(productsFilePath, productJSON);
+		products.forEach( product => {
+			if(product.id === id) {
+				fs.unlink(imagesPath + product.image, (err) => {
+					if(err) throw err;
+					console.log(err);
+				});
+			}
+		});
+		res.redirect('/products');
+		
+	}
+};
+
+module.exports = controller;
---
+++ src/routes/products.js
@@ -0,0 +1,43 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+//Config multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/images/products'));
+    },
+    filename: (req, file, cb) => {
+        const newFilename = 'product-' + Date.now() + path.extname(file.originalname); 
+        cb(null, newFilename);
+    }
+});
+
+const upload = multer({ storage });
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create/', productsController.create); 
+router.post('/', upload.single('imagen'), productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id/', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', upload.single('imagen'), productsController.update); 
+
+
+/*** DELETE ONE PRODUCT ***/ 
+router.delete('/:id', productsController.destroy); 
+
+
+module.exports = router;
